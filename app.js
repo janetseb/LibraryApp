c5f814c53af820c09656bdeeafcbb69f91cdadf9
janet <sjanet2000@gmail.com>
2021-01-09 11:49:31 +0530

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express');
 
 
 const app= express();
-const port =process.env.port || 2000;
+const port =process.env.PORT || 2000;
 
 const nav = [
     {link:"/books",name:"Books"},
